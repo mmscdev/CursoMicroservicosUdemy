64be70c846d0bb627eaa1315f92a9c61fba03f34
mayara merlin <mayaramerlin@hotmail.com>
2022-06-02 08:57:46 -0300

    Criar Controller Person

---
+++ RestWithAspNetUdemy/RestWithAspNetUdemy/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using RestWithAspNetUdemy.Model;
+
+namespace RestWithAspNetUdemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        Person Update(Person person);
+        void Delete(long id);
+        List<Person> FindAll();
+    }
+}

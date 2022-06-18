e87659450b657076aa900103f7871ddb1d24dbb9
mayara merlin <mayaramerlin@hotmail.com>
2022-06-18 18:11:48 -0300

    remove

--- RestWithAspNetUdemy/src/MM.GeekShopping.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MM.GeekShopping.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

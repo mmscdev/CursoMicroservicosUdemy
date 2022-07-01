b8f8892c358efc85d557aa8f273f99cc19d1c933
mayara merlin <mayaramerlin@hotmail.com>
2022-07-01 19:29:51 -0300

    carrinho api

---
+++ RestWithAspNetUdemy/src/MM.GeekShopping.CartAPI/Config/MappingConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using GeekShopping.CartAPI.Data.ValueObjects;
+using GeekShopping.CartAPI.Model;
+
+namespace GeekShopping.CartAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config => 
+            {
+                config.CreateMap<ProductVO, Product>();
+                config.CreateMap<Product, ProductVO>();
+                config.CreateMap<CartHeaderVO, CartHeader>().ReverseMap();
+                config.CreateMap<CartDetailVO, CartDetail>().ReverseMap();
+                config.CreateMap<CartVO, Cart>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+    }
+}

d5340c40e19653924b71c4e7af97dd1f03ac6ad0
mayara merlin <mayaramerlin@hotmail.com>
2022-07-01 20:59:29 -0300

    adicionar carrinho

--- RestWithAspNetUdemy/src/MM.GeekShopping.CartAPI/Config/MappingConfig.cs
+++ RestWithAspNetUdemy/src/MM.GeekShopping.CartAPI/Config/MappingConfig.cs
@@ -8,10 +8,8 @@ namespace GeekShopping.CartAPI.Config
     {
         public static MapperConfiguration RegisterMaps()
         {
-            var mappingConfig = new MapperConfiguration(config => 
-            {
-                config.CreateMap<ProductVO, Product>();
-                config.CreateMap<Product, ProductVO>();
+            var mappingConfig = new MapperConfiguration(config => {
+                config.CreateMap<ProductVO, Product>().ReverseMap();
                 config.CreateMap<CartHeaderVO, CartHeader>().ReverseMap();
                 config.CreateMap<CartDetailVO, CartDetail>().ReverseMap();
                 config.CreateMap<CartVO, Cart>().ReverseMap();

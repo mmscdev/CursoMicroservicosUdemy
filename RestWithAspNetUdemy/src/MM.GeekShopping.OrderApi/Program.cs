f16c17aa1a856636304a6ad32ebd3494c976a0f6
mayara merlin <mayaramerlin@hotmail.com>
2022-07-20 22:05:40 -0300

    Module20

---
+++ RestWithAspNetUdemy/src/MM.GeekShopping.OrderApi/Program.cs
@@ -0,0 +1,40 @@
+using GeekShopping.OrderAPI.Model.Context;
+using GeekShopping.OrderAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options => options.
+    UseMySql(connection,
+            new MySqlServerVersion(
+                new Version(8, 0, 5))));
+
+builder.Services.AddSingleton<IOrderRepository, OrderRepository>();
+
+builder.Services.AddControllers();
+//builder.Services.AddSingleton<IRabbitMQMessageSender, RabbitMQMessageSender>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
